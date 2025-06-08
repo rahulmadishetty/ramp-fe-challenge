0dbb95275268b243c076ffc74ae63d1764c62de5
Rahul Madishetty <rahulmadishetty24@gmail.com>
2025-06-07 20:54:59 -0400

    Fixed Bug 4

--- src/hooks/usePaginatedTransactions.ts
+++ src/hooks/usePaginatedTransactions.ts
@@ -21,8 +21,8 @@ export function usePaginatedTransactions(): PaginatedTransactionsResult {
       if (response === null || previousResponse === null) {
         return response
       }
-
-      return { data: response.data, nextPage: response.nextPage }
+      //Fixed Bug 4: We accumulate data across all pages
+      return { data: [...previousResponse.data, ...response.data], nextPage: response.nextPage }
     })
   }, [fetchWithCache, paginatedTransactions])
 
